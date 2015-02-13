3a60596e508e221fec92f4986fbfb5f8632e4173
Michael Woolnough <michael.woolnough@gmail.com>
2015-02-13 20:36:11 +0000

    Added dark block (blocks light)

---
+++ mw/ferrumwoodblocks/DarkBlock.java
@@ -0,0 +1,53 @@
+package mw.ferrumwoodblocks;
+
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
+
+public class DarkBlock extends Block {
+	public DarkBlock(int blockId) {
+		super(blockId, Material.air);
+		this.setUnlocalizedName("darkBlock")
+		.setCreativeTab(CreativeTabs.tabDecorations)
+		.setLightValue(0)
+		.setLightOpacity(15);
+	}
+	
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+	
+	@Override
+	public int getRenderType() {
+		return -1;
+	}
+	
+	@Override
+	public int damageDropped(int metadata) {
+		return metadata;
+	}
+	
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+	
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+	        return null;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void getSubBlocks(int blockId, CreativeTabs cTabs, List list) {
+		list.add(new ItemStack(blockId, 1, 0));
+	}
+}
--- mw/ferrumwoodblocks/ModFerrumwoodBlocks.java
+++ mw/ferrumwoodblocks/ModFerrumwoodBlocks.java
@@ -28,7 +28,8 @@ public class ModFerrumwoodBlocks {
 		int wallBlockId = Config.get("BlockIds", "WallBlock", 4006).getInt();
 		int doorId = Config.get("BlockIds", "DoorBlock", 4007).getInt();
 		int roadSlopeId = Config.get("BlockIds", "SlopeBlock", 4008).getInt();
-		int sunLightId = Config.get("BlockIds", "LightBlock", 4009).getInt();
+		int lightId = Config.get("BlockIds", "LightBlock", 4009).getInt();
+		int darkId = Config.get("BlockIds", "DarkBlock", 4010).getInt();
 
 		Config.save();
 
@@ -51,7 +52,8 @@ public class ModFerrumwoodBlocks {
 		GameRegistry.registerBlock(new RoadSlope(roadSlopeId), RoadSlopeItem.class, "roadSlope");
 		BlockManipulator.registerManipulator(roadSlopeId, new DefaultManipulators.Bits(12, 8, 4, 0, 12));
 
-		GameRegistry.registerBlock(new LightBlock(sunLightId), BlockItem.class, "lightBlock");
+		GameRegistry.registerBlock(new LightBlock(lightId), BlockItem.class, "lightBlock");
+		GameRegistry.registerBlock(new DarkBlock(darkId), BlockItem.class, "darkBlock");
 
 		Block.blocksList[95] = null;
 		Block.lightValue[95] = 0;
